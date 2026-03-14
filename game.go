7b9b501ca19c15d2bf6d08af61bd50bc0e09202a
zadam <zerenadam7@gmail.com>
2026-03-14 13:44:51 -0400

    tilemap

--- game.go
+++ game.go
@@ -8,3 +8,17 @@ type Game struct {
 	CurrentScene  *Scene
 	Buffer        bytes.Buffer
 }
+
+func NewGame(width, height uint) *Game {
+	return &Game{
+		Width:        width,
+		Height:       height,
+		CurrentScene: nil,
+		Scenes:       []*Scene{},
+		Buffer:       *bytes.NewBuffer(nil),
+	}
+}
+
+func (g *Game) NewScene(s *Scene) {
+	g.Scenes = append(g.Scenes, s)
+}
--- scene.go
+++ scene.go
@@ -1,7 +1,18 @@
 package goscii
 
+import "strings"
+
 type Scene struct {
 	Name    string
 	Tilemap [][]rune
 	Sprites []*Sprite
+}
+
+func TilemapFromString(mapstr string) [][]rune {
+	lines := strings.Split(strings.TrimSpace(mapstr), "\n")
+	tilemap := make([][]rune, len(lines))
+	for y, line := range lines {
+		tilemap[y] = []rune(line)
+	}
+	return tilemap
 }
